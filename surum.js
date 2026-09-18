5e1efee3f30805bd13c902b58882c9b062359679
gamerkrh <dogan-kal@hotmail.com>
2026-09-18 10:01:54 +0300

    Add files via upload

--- surum.js
+++ surum.js
@@ -1,5 +1,5 @@
 window.KRH_SURUM && window.KRH_SURUM({
-  "surum": 1,
+  "surum": 2,
   "url": "https://github.com/gamerkrh/Karahan/blob/main/kazanc-takip.html",
-  "not": "İlk sürüm"
+  "not": "İngilizce dil desteği + cihaz kilidi"
 });
